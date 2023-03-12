58eab25edb5cb5019c56f109c198bd1ffc116220
Desiree Zarco <desireeannzarco7@gmail.com>
2023-03-12 11:18:17 -0700

    Adds myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+const messages = [];
+function writeCards(names, eventNames){
+    for (let i = 0; i < names.length; i++){
+        let message = `Thank you, ${names[i]}, for the wonderful ${eventNames} gift!`;
+        messages.push(message);}
+        
+        return messages
+    }
+
+    function countDown(){
+        let countDown = 10
+        while (countDown > -1){
+            console.log(countDown--)
+        }
+    }
